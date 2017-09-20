abdeefd89c5f1c3d0343f5a172a355cbb6c5fd99
汤阳 <tangyang22007@126.com>
2017-09-20 17:51:50 +0800

    添加todo

--- src/components/addTodo.vue
+++ src/components/addTodo.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>this is addTodo</div>
+  <div class='at-input-wrap'>
+    <div class="at-input-item">
+      <h4>标题</h4>
+      <b-form-input v-model.trim="title"
+                    type="text"
+                    :state="nameState"
+                    placeholder="这里输入标题"></b-form-input>
+      <b-form-feedback>
+        输入字符太短了
+      </b-form-feedback>
+    </div>
+    <div class="at-input-item">
+      <h4>描述</h4>
+      <b-form-input v-model.trim="description"
+                    type="text"
+                    :state="desState"
+                    placeholder="这里输入描述"></b-form-input>
+      <b-form-feedback>
+        多输几个字会死啊
+      </b-form-feedback>
+    </div>
+    <b-button :variant="'primary'" @click = 'loadAction'>
+      添加
+    </b-button>
+    <b-button :variant="'secondary'" @click = 'cleanAction'>
+      清空
+    </b-button>
+  </div>
 </template>
 
 <script>
@@ -7,11 +34,50 @@ export default {
   name: 'addTodo',
   data () {
     return {
+      title: '',
+      description: ''
     }
   },
   created () {
   },
   methods: {
+    loadAction () {
+      if (this.nameState || this.desState) {
+        return
+      }
+      this.axios.post(`${this._config().preurl}savetodo`, {
+        title: this.title,
+        description: this.description
+      }).then((response) => {
+        if (response.data.err) {
+          return Promise.reject()
+        }
+        this.showAlert({
+          msg: '添加成功，记得按时完成哦~',
+          autoClose: true,
+          type: 'success'
+        })
+        this.cleanAction()
+      }).catch(() => {
+        this.showAlert({
+          msg: '接口异常，请联系管理员Token',
+          autoClose: false,
+          type: 'error'
+        })
+      })
+    },
+    cleanAction () {
+      this.title = ''
+      this.description = ''
+    }
+  },
+  computed: {
+    nameState () {
+      return this.title.length > 2 ? null : 'invalid';
+    },
+    desState () {
+      return this.description.length > 4 ? null : 'invalid';
+    }
   },
   mounted () {
   }
@@ -20,4 +86,10 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.at-input-wrap{
+  padding: 5px 10%
+}
+.at-input-item{
+  margin: 10px 0
+}
 </style>
