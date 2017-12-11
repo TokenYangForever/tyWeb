535eaa2387d887ab95390991b888899690cfd658
汤阳 <tangyang22007@126.com>
2017-12-11 14:46:29 +0800

    sudo从数据库取

--- server/methods/getSudo.js
+++ server/methods/getSudo.js
@@ -11,6 +11,12 @@ const getChangeArr = () => {
   return changeArr
 }
 
+const difficulty_map = {
+  '入门': 41,
+  '中等': 48,
+  '困难': 53
+}
+
 const randomArr = (initArray) => {
   let arr = [...initArray]
   let changeArr = getChangeArr()
@@ -28,8 +34,9 @@ const randomArr = (initArray) => {
   return arr
 }
 
-const getQueArr = (arr) => {
-  for (let i = 0; i < 21; i++) {
+const getQueArr = (arr, diff) => {
+  let counts = difficulty_map[diff]
+  for (let i = 0; i < counts; i++) {
     arr = getBlock(arr)
   }
   return arr
@@ -45,33 +52,56 @@ const getBlock = (ar) => {
   }
 }
 
+// module.exports = function (req, res) {
+//   let initArray = [
+//     [1, 2, 3, 4, 5, 6, 7, 8, 9],
+//     [4, 5, 6, 7, 8, 9, 1, 2, 3],
+//     [7, 8, 9, 1, 2, 3, 4, 5, 6],
+//     [2, 1, 4, 3, 6, 5, 8, 9, 7],
+//     [3, 6, 5, 8, 9, 7, 2, 1, 4],
+//     [8, 9, 7, 2, 1, 4, 3, 6, 5],
+//     [5, 3, 1, 6, 4, 2, 9, 7, 8],
+//     [6, 4, 2, 9, 7, 8, 5, 3, 1],
+//     [9, 7, 8, 5, 3, 1, 6, 4, 2]
+//   ]
+//   // req.query 请求参数
+//   let backdata = {
+//   }
+//   // 获得打乱后的数组
+//   let getArr = randomArr(initArray)
+//   let result = []
+//   for (let i = 0; i < getArr.length; i++) {
+//     result = [...result, ...getArr[i]]
+//   }
+//   let quesArr = getQueArr(getArr, req.query.difficulty)
+//   let quesResult = []
+//   for (let i = 0; i < quesArr.length; i++) {
+//     quesResult = [...quesResult, ...getArr[i]]
+//   }
+//   backdata.quesArr = quesResult
+//   backdata.result = result
+//   res.send(backdata)
+// }
+let connection = require('../config').connection
+
 module.exports = function (req, res) {
-  let initArray = [
-    [1, 2, 3, 4, 5, 6, 7, 8, 9],
-    [4, 5, 6, 7, 8, 9, 1, 2, 3],
-    [7, 8, 9, 1, 2, 3, 4, 5, 6],
-    [2, 1, 4, 3, 6, 5, 8, 9, 7],
-    [3, 6, 5, 8, 9, 7, 2, 1, 4],
-    [8, 9, 7, 2, 1, 4, 3, 6, 5],
-    [5, 3, 1, 6, 4, 2, 9, 7, 8],
-    [6, 4, 2, 9, 7, 8, 5, 3, 1],
-    [9, 7, 8, 5, 3, 1, 6, 4, 2]
-  ]
   // req.query 请求参数
   let backdata = {
+    result: []
   }
-  // 获得打乱后的数组
-  let getArr = randomArr(initArray)
-  let result = []
-  for (let i = 0; i < getArr.length; i++) {
-    result = [...result, ...getArr[i]]
-  }
-  let quesArr = getQueArr(getArr)
-  let quesResult = []
-  for (let i = 0; i < quesArr.length; i++) {
-    quesResult = [...quesResult, ...getArr[i]]
-  }
-  backdata.quesArr = quesResult
-  backdata.result = result
-  res.send(backdata)
+  let difficulty = req.query.difficulty || 'Easy'
+  let qstring = `SELECT Data from sudoTable where difficulty = '${difficulty}'`
+  connection.query(qstring, function (err, rs) {
+    if (err) {
+      console.log(`[query] - :${err}`)
+      backdata.err = err
+      res.send(backdata)
+      return
+    }
+    let index = Math.floor(Math.random() * rs.length)
+    let Data = rs[index].Data.split(',')
+    Data = Data.map((item) => item === '0' ? '' : Number(item))
+    backdata.result = Data
+    res.send(backdata)
+  })
 }
