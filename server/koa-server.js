d540957754d5503302d5c8986881364fb1cbcfce
汤阳 <tangyang22007@126.com>
2018-04-28 15:38:08 +0800

    ？？

---
+++ server/koa-server.js
@@ -0,0 +1,35 @@
+const Koa = require('koa')
+const Router = require('koa-router');
+const app = new Koa()
+const router = new Router()
+const about = ctx => {
+	console.log(ctx.request)
+  ctx.response.type = 'html'
+  ctx.response.body = '<a href="/">Index Page</a>'
+}
+
+const main = ctx => {
+  ctx.response.body = 'Hello World'
+}
+
+const redirect = ctx => {
+  ctx.response.redirect('/')
+  ctx.response.body = '<a href="/">Index Page</a>'
+}
+
+// 中间件
+const logger = (ctx, next) => {
+  console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`)
+  next()
+}
+
+router.get('/', main).get('/about', about).get('/redirect', redirect)
+
+app.use(logger)
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
+// app.use(route.get('/', main))
+// app.use(route.get('/about', about))
+// app.use(route.get('/redirect', redirect))
+app.listen(3001)
