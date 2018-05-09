721636477ee0524c14d573f0e941220f34ce092a
tangyangyang <tangyang22007@126.com>
2018-05-09 14:00:16 +0800

    新增koa-session

--- server/koa-server.js
+++ server/koa-server.js
@@ -1,15 +1,46 @@
 const Koa = require('koa')
 const Router = require('koa-router');
+const session = require('koa-session')
 const app = new Koa()
 const router = new Router()
 const compose = require('koa-compose')
 const about = ctx => {
-	console.log(ctx.request)
   ctx.response.type = 'html'
   ctx.response.body = '<a href="/">Index Page</a>'
 }
 
+app.keys = ['some secret hurr'];
+
+const CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 86400000,
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+  renew: false, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+};
+
+app.use(session(CONFIG, app));
+// or if you prefer all default config, just use => app.use(session(app));
+
+app.use(ctx => {
+  // ignore favicon
+  if (ctx.path === '/favicon.ico') return;
+
+  let n = ctx.session.views || 0;
+  ctx.session.views = ++n;
+  console.log(ctx.session.views)
+});
+
 const main = ctx => {
+  // 这样获取从浏览器得到的cookie
+  ctx.cookie.get('cookieA')
+  // 这样设置传递给浏览器的cookie
+  ctx.cookie.set('cookieA', 'a').set('cookieB', 'b')
   ctx.response.body = 'Hello World'
 }
 
@@ -20,7 +51,6 @@ const redirect = ctx => {
 
 // 中间件
 const logger = (ctx, next) => {
-  console.log(`${Date.now()} ${ctx.request.method} ${ctx.request.url}`)
   next()
 }
 
